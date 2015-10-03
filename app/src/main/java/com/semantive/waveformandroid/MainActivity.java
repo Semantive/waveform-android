b1b64c2436aaf2b3ed0c0b829eac66dcecf69bbd
Anna Stępień <anna.stepien@semantive.com>
2015-10-03 19:02:49 +0200

    Updated MainActivity.

--- app/src/main/java/com/semantive/waveformandroid/MainActivity.java
+++ app/src/main/java/com/semantive/waveformandroid/MainActivity.java
@@ -2,9 +2,7 @@ package com.semantive.waveformandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.*;
 import com.semantive.waveformandroid.waveform.WaveformFragment;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -17,29 +15,6 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new CustomWaveformFragment())
                     .commit();
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public static class CustomWaveformFragment extends WaveformFragment {
